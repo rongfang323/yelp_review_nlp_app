fedb1fc5b1a4109125fbe1c06d9a26b9190982f4
rongfang323 <rong.fang323@gmail.com>
2020-09-02 19:31:03 -0700

    make it better

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ import os
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/favicon.ico', methods = ['GET'])
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='img/favicon.png')
+
+@app.route('/', methods = ['GET'])
 def index():
 
     return render_template('index.html')
@@ -318,10 +323,7 @@ def about():
 def personal_profile():
     return render_template('personal_profile.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='img/favicon.png')
+
 
 
 if __name__ == '__main__':
